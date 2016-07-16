af49660b3ed400f005d842e9c83153e37bc94ccc
Ran <lr_naril@163.com>
2016-07-16 19:08:43 +0800

    add recently viewed and brand logo

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -11,3 +11,17 @@ Deface::Override.new(
   insert_after: "#product_description[data-hook], [data-hook='product_description']",
   partial: 'spree/shared/add_recently_viewed_products'
 )
+
+Deface::Override.new(
+  virtual_path: 'spree/orders/show',
+  name: 'add_recently_viewed_products_to_orders_show',
+  insert_after: "#order_summary[data-hook], [data-hook='order_summary']",
+  partial: 'spree/shared/add_recently_viewed_products'
+)
+
+Deface::Override.new(
+  virtual_path: 'spree/orders/edit',
+  name: 'add_recently_viewed_products_to_cart_show',
+  insert_after: "#cart-container[data-hook], [data-hook='cart-container']",
+  partial: 'spree/shared/add_recently_viewed_products'
+)
